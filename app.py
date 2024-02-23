2d9780b912e843e2b9cefabd38a8110b61627e75
Max Jeon <maxjeon97@gmail.com>
2024-02-22 17:13:23 -0800

    Complete exercise

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
+RESPONSE_KEY = "response"
+
 # satisfaction_survey = Survey(
 #     "Customer Satisfaction Survey",
 #     "Please fill out a survey about your experience with us.",
@@ -20,26 +22,41 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
 
 
 @app.get("/")
-def survey_start():
-    """generates start page for survey"""
+def start_survey():
+    """Generates start page for survey"""
 
     return render_template("survey_start.html", survey=survey)
 
 
 @app.post("/begin")
 def begin():
-    """redirects to first question page upon pressing start button"""
+    """Redirects to first question page upon pressing start button"""
+
+    session[RESPONSE_KEY] = []
 
     return redirect("/questions/0")
 
 
 @app.get("/questions/<int:number>")
-def question(number):
+def handle_question(number):
     """displays question based on its url parameter number"""
+
+    responses = session.get(RESPONSE_KEY)
+
+    if responses is None:
+        return redirect("/")
+
+    if len(responses) == len(survey.questions):
+        flash("You already completed this survey!")
+        return redirect('/thank-you')
+
+    if number != len(responses):
+        flash("Answer the questions in order!")
+
+        return redirect(f"/questions/{len(responses)}")
 
     question = survey.questions[number]
     return render_template("question.html", question=question)
@@ -50,16 +67,28 @@ def handle_answer():
     """handles question submission, redirects to thank you
     page when all questions have been answered"""
 
+    responses = session[RESPONSE_KEY]
     responses.append(request.form["answer"])
+    session[RESPONSE_KEY] = responses
 
     if len(responses) == len(survey.questions):
-        return redirect('/thankyou')
+        return redirect('/thank-you')
 
     return redirect(f"/questions/{len(responses)}")
 
 
-@app.get('/thankyou')
+@app.get('/thank-you')
 def thank_user():
     """thanks user for completing survey and shows responses"""
+
+    responses = session.get(RESPONSE_KEY)
+
+    if responses is None:
+        flash("You haven't answered any of the questions!")
+        return redirect("/")
+
+    if len(responses) != len(survey.questions):
+        flash("You haven't completed the survey!")
+        return redirect(f"/questions/{len(responses)}")
 
     return render_template("completion.html", questions=survey.questions, responses=responses)
